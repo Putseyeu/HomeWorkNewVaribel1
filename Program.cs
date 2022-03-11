ea00f342c6ad33e98e13bc28def53fe9f8868c21
Putseyeu <putseyeu.a.d@gmail.com>
2022-03-11 18:56:38 +0100

    step1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2Veribal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Франция";
+            string country = "Париж";
+            ShowInfo(country, city);
+            (country, city) = (city, country);
+            ShowInfo(country, city);
+        }
+
+        static void ShowInfo(string country, string city)
+        {
+            Console.WriteLine($"Столицей {country} является {city}");
+        }
+    }
+}
